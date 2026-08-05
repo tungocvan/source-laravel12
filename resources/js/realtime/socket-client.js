8c69deebafb0c9d514fa8fcad7bc113b01caf833
tungocvan <tungocvan@users.noreply.github.com>
2026-08-05 12:49:37 +0700

    Allow Socket.IO polling fallback

--- resources/js/realtime/socket-client.js
+++ resources/js/realtime/socket-client.js
@@ -9,8 +9,8 @@ const socketHost = window.APP_CONFIG?.realtime?.url
 
 window.socket ??= io(socketHost, {
     path: '/socket.io',
-    transports: ['websocket'],
-    upgrade: false,
+    transports: ['polling', 'websocket'],
+    upgrade: true,
     reconnection: true,
     reconnectionAttempts: 10,
     reconnectionDelay: 1000,
